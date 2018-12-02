d7a36aa30d1ed824ba0797dac971188966f7e6c4
Charlie Peters <chpeters13@gmail.com>
2018-12-02 17:45:44 -0500

    add api.js

---
+++ api/lib/api.js
@@ -0,0 +1,8 @@
+const getDashboardJson = {
+  data: [],
+}
+
+const addDashboardJson = {}
+
+export const getDashboards = () => Promise.resolve(getDashboardJson)
+export const addDashboard = () => Promise.resolve(addDashboardJson)
