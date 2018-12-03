5d412faeef1c7fd1a9aaef54113a533cbd4a8e55
Charlie Peters <chpeters13@gmail.com>
2018-12-02 21:15:48 -0500

    use rebass card

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { ResponsiveBar } from '@nivo/bar'
-import Card from './Card'
+import { Card } from 'rebass'
 
 /**
  * Expects data of form [{key: string, value: number}...]
@@ -9,7 +9,13 @@ import Card from './Card'
  * @param {enum} type
  */
 const Dashboard = ({ title, data, type }) => (
-  <Card>
+  <Card
+    borderRadius={10}
+    boxShadow="0px 0px 12px 1px rgba(0, 0, 0, 0.2)"
+    width={800}
+    height={450}
+    mt={50}
+  >
     <h2>Hello world</h2>
     <ResponsiveBar data={data} height={450} colorBy="index" />
   </Card>
