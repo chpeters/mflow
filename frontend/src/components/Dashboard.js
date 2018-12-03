f0b8b0f56c9b8518c23ccd04fa06b5e59f360123
zackr <reiter.z@husky.neu.edu>
2018-12-02 23:18:20 -0500

    pie chart support and sample data

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { ResponsiveBar } from '@nivo/bar'
 import { ResponsiveLine } from '@nivo/line'
+import { ResponsivePie } from '@nivo/pie'
 import ShadowCard from './ShadowCard'
 
 const lineData = [
@@ -13,6 +14,39 @@ const lineData = [
         data: [{x: 1,y: 2},{x: 4,y: 4},{x: 6,y: 1}]
     }
 ];
+
+const pieData = [
+  {
+    "id": "erlang",
+    "label": "erlang",
+    "value": 321,
+    "color": "hsl(52, 70%, 50%)"
+  },
+  {
+    "id": "lisp",
+    "label": "lisp",
+    "value": 331,
+    "color": "hsl(87, 70%, 50%)"
+  },
+  {
+    "id": "scala",
+    "label": "scala",
+    "value": 314,
+    "color": "hsl(296, 70%, 50%)"
+  },
+  {
+    "id": "c",
+    "label": "c",
+    "value": 385,
+    "color": "hsl(258, 70%, 50%)"
+  },
+  {
+    "id": "stylus",
+    "label": "stylus",
+    "value": 65,
+    "color": "hsl(258, 70%, 50%)"
+  }
+]
 
 /**
  * ResponsiveBar:
@@ -30,9 +64,15 @@ const Dashboard = ({ title, data, type }) => (
       <h2>Hello world</h2>
       <ResponsiveBar data={data} height={450} colorBy="index" />
     </ShadowCard>
+    <br />
     <ShadowCard>
       <h2>Hello world2</h2>
-      <ResponsiveLine data={lineData} />
+      <ResponsiveLine data={lineData} height={450}/>
+    </ShadowCard>
+    <br />
+    <ShadowCard>
+      <h2>Hello world3</h2>
+      <ResponsivePie data={pieData} height={450}/>
     </ShadowCard>
   </div>
 )
