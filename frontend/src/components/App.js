43850608ad479ccee705010f1ad9c1c3c456d712
Charlie Peters <chpeters13@gmail.com>
2018-12-02 20:52:18 -0500

    add rebass

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,13 +1,20 @@
 import React from 'react'
 import Dashboard from './Dashboard'
+import { Flex, Box } from 'rebass'
 
 const App = () => (
-  <div>
-    <h1>Hello world</h1>
-    <Dashboard
-      data={[{ id: 'test', value: 100 }, { id: 'test2', value: 200 }]}
-    />
-  </div>
+  <>
+    <Flex justifyContent="center">
+      <Box>
+        <h1>Hello world</h1>
+      </Box>
+    </Flex>
+    <Flex justifyContent="center">
+      <Dashboard
+        data={[{ id: 'test', value: 100 }, { id: 'test2', value: 200 }]}
+      />
+    </Flex>
+  </>
 )
 
 export default App
