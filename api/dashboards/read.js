54cd15dbd38c58d94c92395e70d5b1a3214b91e2
Charlie Peters <chpeters13@gmail.com>
2018-12-08 14:48:27 -0500

    moar logs

--- api/dashboards/read.js
+++ api/dashboards/read.js
@@ -14,11 +14,13 @@ export default pico(
   withCors((req) => {
     console.log(req);
     const { id } = url.parse(req.url, true).query;
+    console.log(id);
     connection.query(`select * from dashboard where user_id = ${id}`, (error, results, fields) => {
       if (!error && results !== null) {
         console.log('results');
         return res(results);
       }
+      console.log('error');
       return res(error, 400);
     });
   }),
