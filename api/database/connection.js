c1214a871b489e24fee717483d23d360dd4f0c94
Charlie Peters <chpeters13@gmail.com>
2018-12-08 12:02:42 -0500

    add logging

--- api/database/connection.js
+++ api/database/connection.js
@@ -1,4 +1,9 @@
 var mysql = require('mysql')
+
+console.log(process.env.HOST)
+console.log(process.env.USER)
+console.log(process.env.PASSWORD)
+console.log(process.env.DATABASE)
 var connection = mysql.createConnection({
   host: process.env.HOST,
   user: process.env.USER,
