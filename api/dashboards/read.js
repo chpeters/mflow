4526d69a1ab4b5a9d2b8ee0e57cb08e9d85a8363
Charlie Peters <chpeters13@gmail.com>
2018-12-08 12:43:15 -0500

    should work now

--- api/dashboards/read.js
+++ api/dashboards/read.js
@@ -1,5 +1,5 @@
-const connection = require('../database/connection.js')
-const url = require('url')
+const url = require('url');
+const connection = require('../database/connection.js');
 // Get all dashboards for a user.
 // This will be the way we use this endpoint because
 // a user should see all their dashboards after they log in
@@ -7,33 +7,26 @@ const url = require('url')
 // save the data there and use it, so we won't need to query
 // for a specific dashboard.
 module.exports = (req, res) => {
-  console.log(process.env.HOST)
-  console.log(process.env.USER)
-  console.log(process.env.PASSWORD)
-  console.log(process.env.DATABASE)
-  console.log('made it')
-  connection.connect(err => console.log(err))
-  res.setHeader('Access-Control-Allow-Origin', '*')
-  res.setHeader(
-    'Access-Control-Allow-Methods',
-    'GET, POST, OPTIONS, PUT, PATCH, DELETE'
-  )
+  console.log(process.env.HOST);
+  console.log(process.env.USER);
+  console.log(process.env.PASSWORD);
+  console.log(process.env.DATABASE);
+  console.log('made it');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.setHeader(
     'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept, x-auth'
-  )
+    'Origin, X-Requested-With, Content-Type, Accept, x-auth',
+  );
 
-  const { id } = url.parse(req.url, true).query
-  console.log(`id: ${id}`)
-  connection.query(
-    `select * from dashboard where user_id = ${user_id}`,
-    function(error, results, fields) {
-      if (!error && results !== null) {
-        res.end(results)
-      } else {
-        res.statusCode = 400
-        res.end()
-      }
+  const { id } = url.parse(req.url, true).query;
+  console.log(`id: ${id}`);
+  connection.query(`select * from dashboard where user_id = ${id}`, (error, results, fields) => {
+    if (!error && results !== null) {
+      res.end(results);
+    } else {
+      res.statusCode = 400;
+      res.end();
     }
-  )
-}
+  });
+};
